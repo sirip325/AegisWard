ddd8584e42860773a915183b3742124da19e6ad0
sirip325 <s.ergeyklim08@gmail.com>
2025-05-30 19:25:59 +0300

    add StatsUI to the Scene

--- Assets/AegisWard/Scripts/Player/View/StatsUI.cs
+++ Assets/AegisWard/Scripts/Player/View/StatsUI.cs
@@ -1,9 +1,31 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class StatsUI : MonoBehaviour
 {
+    private VisualElement root;
     private ProgressBar _progressBar;
+
+    private void OnEnable()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+    }
     
-    
+    private void Start()
+    {
+        _progressBar = root.Q<ProgressBar>("ManaBar");
+    }
+
+    private void Update()
+    {
+        TestBar();
+    }
+    public void TestBar()
+    {
+        if (_progressBar != null)
+        {
+            _progressBar.value = Mathf.Clamp(_progressBar.value + Time.deltaTime, 0, 100);
+        }
+    }
 }
--- Assets/AegisWard/Scripts/Player/ViewModel/StatsViewModel.cs
+++ Assets/AegisWard/Scripts/Player/ViewModel/StatsViewModel.cs
@@ -1,13 +1,21 @@
+using AegisWard.Scripts.Player.Model;
 using UnityEngine;
 using VContainer;
 
 public class StatsViewModel
 {
     private StatsUI _statsUI;
+    private PlayerStats _playerStats;
 
     [Inject]
-    public StatsViewModel(StatsUI statsUI)
+    public StatsViewModel(StatsUI statsUI, PlayerStats playerStats)
     {
         _statsUI = statsUI;
+        _playerStats = playerStats;
+    }
+
+    public void Init()
+    {
+        
     }
 }
